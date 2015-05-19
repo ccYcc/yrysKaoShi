8b9c15b4f8b611a20ecd1e075afc8e687159f7d0
cxl <chenxiaolong115@gmail.com>
2015-05-19 15:34:36 +0800

    CxlTestController

--- src/com/ccc/test/controller/CxlTestController.java
+++ src/com/ccc/test/controller/CxlTestController.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ccc.test.service.interfaces.IPaperService;
+
 /**
  * @author cxlTest
  *
